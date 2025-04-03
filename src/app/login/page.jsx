405c227e804b0ab15a76eb31bbf41395cee3fd00
mdimranictiu <imranictiu11@gmail.com>
2025-04-03 09:10:52 +0600

    Project Start

---
+++ src/app/login/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <div>
+        <div className='flex flex-col justify-center min-h-screen items-center'>
+        
+         <div className='bg-white shadow-2xl rounded-lg w-full max-w-sm p-10'>
+         <h2>Chatify</h2>
+         </div>
+        </div>
+    </div>
+  )
+}
